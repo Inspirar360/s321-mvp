94999e1e2641dd53284ab3ef57da26dfa9c65100
Inspirar360 <inspirarnegocios360@gmail.com>
2025-08-09 20:39:57 -0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,1 +1,35 @@
+const CACHE_NAME = 's321-mvp-v1';
+const ASSETS = [
+  '/s321-mvp/',
+  '/s321-mvp/index.html',
+  '/s321-mvp/icons/icon-192.png',
+  '/s321-mvp/icons/icon-512.png'
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached ||
+      fetch(event.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then((c) => c.put(event.request, copy));
+        return resp;
+      }).catch(() => caches.match('/s321-mvp/index.html'))
+    )
+  );
+});
